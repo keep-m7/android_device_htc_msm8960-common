86e41bd090b0a3ab3a30853b266e4e1936c5123d
Bruno Martins <bgcngm@gmail.com>
2026-01-18 22:51:07 +0300

    msm8960-common: camera: Remove obsolete legacy HAL1 props
     * Only TARGET_HAS_LEGACY_CAMERA_HAL1 board flag is needed now.
    
    Change-Id: I266445e7fd8bc84337d56333c8e3d3f536039fd1

--- system_prop.mk
+++ system_prop.mk
@@ -24,8 +24,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1 \
-    media.stagefright.legacyencoder=true \
-    media.stagefright.less-secure=true \
     camera.disable_treble=true \
     debug.camcorder.disablemeta=true
 
