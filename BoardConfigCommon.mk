bf0e924c7f29aac6ea88c22e59d2dda8e94328fd
CyberWalkMaN <firefox883@gmail.com>
2026-01-18 22:31:05 +0300

    msm8960-common: Remove deprecated HAVE_ADRENO_SOURCE flag
    Change-Id: Idc95aaaef8268c1a15eb1830e394045f550034f1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -94,7 +94,6 @@ BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
 WITH_LINEAGE_CHARGER := false
 
 # Graphics
-HAVE_ADRENO_SOURCE := false
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 
 # Extended Filesystem Support
