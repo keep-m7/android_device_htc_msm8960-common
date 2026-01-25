13c6eeef01dbcbc459f5fced0e18bd3234d107e2
Sean Callanan <spyffe@google.com>
2026-01-25 17:46:03 +0300

    msm8960-common: Switch to IMapper 2.1
    Test: display CTS
    Bug: 62535446
    Change-Id: I2d6d0d118014eb2b99a6832b4c900ae21c062517

--- msm8960.mk
+++ msm8960.mk
@@ -108,7 +108,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
     copybit.msm8960 \
     gralloc.msm8960 \
     hwcomposer.msm8960 \
